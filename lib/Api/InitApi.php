0c33075b7336d0e7f36bae4074e5f97d5c97035e
chatway <61035342+chatway@users.noreply.github.com>
2022-10-03 16:30:16 +0500

    Update InitApi.php

--- lib/Api/InitApi.php
+++ lib/Api/InitApi.php
@@ -344,7 +344,7 @@ class InitApi
             $headers
         );
 
-        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        $query = \GuzzleHttp\Psr7\Query::build($queryParams);
         return new Request(
             'GET',
             $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
